f26dd5980ec674e567e5dcb44c11eea22c0b7d32
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-21 10:21:36 -0400

    Foundation update

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 RestaurantRails::Application.routes.draw do
-get '/cuisine/:id' => 'cuisines#show', as: 'cuisine'
+get '/cuisines/:id' => 'cuisines#show', as: 'cuisine'
 get '/cuisines' => 'cuisines#index', as: 'cuisines'
 
 get '/pages/' => 'pages#index', as: 'home'
 post '/pages/' => 'pages#search', as: 'search'
 
-get '/restaurant/:id' => 'restaurants#show', as: 'restaurant'
+get '/restaurants/:id' => 'restaurants#show', as: 'restaurant'
 get '/restaurants' => 'restaurants#index', as: 'restaurants'
 post 'restaurants' => 'restaurants#index'
 
-get '/violation/:id' => 'violations#show', as: 'violation'
+get '/violations/:id' => 'violations#show', as: 'violation'
 get '/violations' => 'violations#index', as: 'violations'
 
-get '/zipcode/:id' => 'zipcodes#show', as: 'zip'
+get '/zipcodes/:id' => 'zipcodes#show', as: 'zip'
 
 
 delete '/sessions' => 'sessions#destroy'
