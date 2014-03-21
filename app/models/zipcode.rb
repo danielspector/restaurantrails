32a5acf950c9573689a91064eedfd0e5189817d7
asullivan210 <ariellejsullivan@gmail.com>
2014-03-21 13:52:11 -0400

    spacing to look nicer

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -11,19 +11,19 @@ class Zipcode < ActiveRecord::Base
 
 
   def most_common_vios
-    all_vios = self.restaurants.collect do |r|
-      r.violations
-    end.flatten
+    all_vios =  self.restaurants.collect do |r|
+                  r.violations
+                end.flatten
     
     frequencies = Hash.new(0)
     all_vios.each{|key| frequencies[key] += 1}
+    
     freq_array = frequencies.values.sort
 
-    first = freq_array[-1]
-    third = freq_array[-3]
+    first  = freq_array[-1]
+    third  = freq_array[-3]
     second = freq_array[-2]
-    
-    top = [first, second, third]
+    top    = [first, second, third]
 
     i = 2
     next_highest = freq_array[-i - 1]
@@ -32,6 +32,7 @@ class Zipcode < ActiveRecord::Base
       top <<  next_highest
       i += 1
     end
+
     top.collect do |num|
       frequencies.assoc(frequencies.key(num))
     end
