e72004d339c3828abaea9e8c0b2277883f767ef4
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-03-21 15:40:58 -0400

    Got favorites working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,19 +15,7 @@ class RestaurantsController < ApplicationController
   def add_restaurant
     @user = current_user
     @user.restaurants << Restaurant.find(params[:restaurant_id])
-    # type = params[:type]
-    # if type == "favorite"
-    #   current_user.favorites << @recipe
-    #   redirect_to :back, notice: 'You favorited #{@recipe.name}'
-
-    # elsif type == "unfavorite"
-    #   current_user.favorites.delete(@recipe)
-    #   redirect_to :back, notice: 'Unfavorited #{@recipe.name}'
-
-    # else
-    #   # Type missing, nothing happens
-    #   redirect_to :back, notice: 'Nothing happened.'
-    # end
+    redirect_to @user
   end
 
   def index
