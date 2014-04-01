e8091413444e913446f1fb61e14b0a4cfcbbf0f3
asullivan210 <ariellejsullivan@gmail.com>
2014-04-01 14:41:26 -0400

    empty watchlists

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(document).on("click", ".tiny.alert.round", function(){
     if($(this).parents("div.row:not(.whole)").children(".row").length == 1){
-      $(this).parents("div.row:not(.whole)").children("h3").append("<p class='empty'>You have no zipcodes on your watchlist.</p>")
+      $(this).parents("div.row:not(.whole)").children("h3").append("<p class='empty'>Empty</p>")
     }
     $(this).closest(".whole").remove();
   });
