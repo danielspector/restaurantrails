74f77ca7eabfb21cc282c1ba02f53a9f92efdde8
asullivan210 <ariellejsullivan@gmail.com>
2014-04-01 17:06:01 -0400

    user show

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $(document).on("click", ".tiny.alert.round", function(){
     if($(this).parents("div.row:not(.whole)").children(".row").length == 1){
-      $(this).parents("div.row:not(.whole)").children("h3").append("<p class='empty'>Empty</p>")
+      $(this).parents("div.row:not(.whole)").children("h3").after("<p class='empty'>Empty</p>")
     }
     $(this).closest(".whole").remove();
   });
