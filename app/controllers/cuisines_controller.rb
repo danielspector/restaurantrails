b2140382c0fa74091bf381a9738a9209583ebb04
asullivan210 <ariellejsullivan@gmail.com>
2014-04-03 14:15:18 -0400

    dry up controllers

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,12 +2,6 @@ class CuisinesController < ApplicationController
   before_action :set_cuisine, only: [:show]
 
   def show
-    # if Cuisine.exists?(id: @cuis_id)
-    #   session[:error] = false
-    # else
-    #   session[:error] = true
-    #   redirect_to(pages_path)
-    # end
   end  
 
   def index
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,24 +1,22 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show]
+  before_action :set_user, only: [:add_restaurant, :remove_restaurant]
   before_action :login_required, :only => [:add_restaurant]
 
   def show
   end
 
   def add_restaurant
-    @user = current_user
     r = Restaurant.find(params[:id])
-    @user.restaurants << r if !@user.restaurants.include?(r)
+    @user.restaurants << r
   end
 
   def remove_restaurant
-    @user = current_user
     @user.user_restaurants.where(restaurant_id: params[:id]).destroy_all
   end
 
   def index
-    if params[:q]
-      if params[:q][:name_cont].length < 3
+    if params[:q] && params[:q][:name_cont].length < 3
         flash[:notice] = "Your search must be at least 3 characters long!"
         params.delete(:q)
       end
@@ -28,6 +26,10 @@ class RestaurantsController < ApplicationController
   end
   
   private
+
+  def set_user
+    @user = current_user
+  end
 
   def set_restaurant
     @restaurant = Restaurant.friendly.find(params[:id]) 
